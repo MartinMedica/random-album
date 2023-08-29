8ff7f0fc4247978018cd5521df4f704618f3f7aa
martin medica <martin.medica@gmail.com>
2023-08-28 23:58:02 -0300

    check for window

--- lib/favorites.ts
+++ lib/favorites.ts
@@ -1,16 +1,24 @@
 'use client';
 import { Album } from './albums';
 
-export const getFavoritesAlbums = () =>
-	JSON.parse(
+export const getFavoritesAlbums = () => {
+	if (!window) {
+		return [];
+	}
+
+	return JSON.parse(
 		window.localStorage.getItem('favorites') || JSON.stringify({ albums: [] })
 	).albums;
+};
 
 export const addAlbumToFavorites = (album: Album) => {
 	const favorites = getFavoritesAlbums();
 
 	favorites.push(album);
 
+	if (!window) {
+		return;
+	}
 	window.localStorage.setItem(
 		'favorites',
 		JSON.stringify({ albums: favorites })
@@ -21,6 +29,10 @@ export const removeAlbumFromFavorites = (album: Album) => {
 	let favorites = getFavoritesAlbums().filter(
 		(fav: Album) => fav.id !== album.id
 	);
+
+	if (!window) {
+		return;
+	}
 
 	window.localStorage.setItem(
 		'favorites',
--- src/app/album/[id]/Reddirect.tsx
+++ src/app/album/[id]/Reddirect.tsx
@@ -11,7 +11,9 @@ export default function ReddirectAlbum({ albums }: { albums: Album[] }) {
 			className="m-5 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			onClick={() => {
 				const album: Album = getRandomAlbum(albums);
-				window.location.assign(`${album.id}`);
+				if (window) {
+					window.location.assign(`${album.id}`);
+				}
 			}}
 		>
 			Get Random Album
